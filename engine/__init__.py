b6c677eb408c6d2eba0344f05568506b40db3a4c
Tuomas Lähteenmäki <lahtis@gmail.com>
2026-07-26 17:53:43 +0300

    Remove old engine directory, moved to shl/engine

--- engine/__init__.py
+++
@@ -1,6 +0,0 @@
-from .core import LocalizationEngine
-from .ai_translation import AITranslator
-from .localizer import Localizer
-from .template_localizer import TemplateLocalizer
-
-__all__ = ['LocalizationEngine', 'AITranslator', 'Localizer', 'TemplateLocalizer']
